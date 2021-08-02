a9742d5bb2fd8a4f255d334ae7a291738ab23c41
flashdreiv <dreivan.orprecio@gmail.com>
2021-08-02 23:17:53 +0800

    ---

--- laz.py
+++ laz.py
@@ -15,6 +15,7 @@ opts = webdriver.ChromeOptions()
 opts.headless = True
 
 driver = webdriver.Chrome(options=opts)
+
 driver.get(url)
 
 
@@ -29,21 +30,10 @@ try:
     )
     address = address.text.split(",")
     total = main.find_element_by_class_name("_33O9dg0j").text.replace("reviews", "")
-
-    hotel_list = []
-    hotel = {
-        "Property": title.text,
-        "Address": address[0],
-        "City": address[1],
-        "State": "null",
-        "Title": driver.title,
-        "Url": driver.current_url,
-        "Total # of Reviews": total,
-    }
-    for i in range(2):
+    review_list = []
+    while True:
         # Get Reviews
-        # reviews = driver.find_elements_by_xpath("//div[@class='_2wrUUKlw _3hFEdNs8']")
-        reviews = WebDriverWait(driver, 10).until(
+        reviews = WebDriverWait(driver, 100).until(
             EC.presence_of_all_elements_located(
                 (By.XPATH, "//div[@class='_2wrUUKlw _3hFEdNs8']")
             )
@@ -51,20 +41,16 @@ try:
         # Toggle Review Details
         # All Element
         read_more = (
-            WebDriverWait(driver, 10)
+            WebDriverWait(driver, 100)
             .until(
                 EC.element_to_be_clickable(
                     (
                         By.XPATH,
-                        '//*[@id="component_14"]/div/div[3]/div[7]/div[2]/div[3]/div[1]/div[2]/div/span[1]',
+                        '//div[@class="XUVJZtom"]/span[contains(text(),"Read")]',
                     )
                 )
             )
             .click()
-        )
-        # Wait for the reload of element then grab it again
-        read_less = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, '//span[@class="_1fSlsEgr"]'))
         )
         for review in reviews:
             try:
@@ -72,15 +58,22 @@ try:
             except:
                 location = "None"
 
-            text = review.find_element_by_tag_name("q").text
-            date_of_stay = review.find_element_by_class_name("_34Xs-BQm").text.split(
-                ":"
-            )[1]
-
+            try:
+                text = review.find_element_by_tag_name("q").text
+            except:
+                text = "none"
+            try:
+                date_of_stay = review.find_element_by_class_name(
+                    "_34Xs-BQm"
+                ).text.split(":")[1]
+            except:
+                date_of_stay = "none"
             trip_type = ""
             room_tip = ""
             try:
-                trip_type = review.find_element_by_class_name("_2bVY3aT5").text
+                trip_type = review.find_element_by_class_name("_2bVY3aT5").text.replace(
+                    "Trip type:", ""
+                )
             except:
                 trip_type = "none"
             try:
@@ -88,7 +81,6 @@ try:
                 room_tip = room_tip.replace("Room Tip:", "")
             except:
                 room_tip = "none"
-
             overall_rating = review.find_element_by_class_name("nf9vGX55")
             overall_rating = overall_rating.find_element_by_tag_name(
                 "span"
@@ -96,7 +88,6 @@ try:
             overall_rating = int(
                 overall_rating.replace("ui_bubble_rating bubble_", "")[0]
             )
-
             rating_details = review.find_elements_by_class_name("_3ErKuh24")
             # Rate Types
             rate_types = {}
@@ -107,8 +98,14 @@ try:
                     "ui_bubble_rating bubble_", ""
                 )[0]
                 rate_types[label] = rate
-
             review_item = {
+                "Property": title.text,
+                "Address": address[0],
+                "City": address[1],
+                "State": "null",
+                "Title": driver.title,
+                "Url": driver.current_url,
+                "Total # of Reviews": total,
                 "location": location,
                 "review": text,
                 "Date of Stay": date_of_stay,
@@ -116,18 +113,26 @@ try:
                 "Room Tip": room_tip,
                 "Overall Rating": overall_rating,
             }
-
-            hotel.update(review_item)
-            hotel.update(rate_types)
-            hotel_list.append(hotel)
-        next = (
-            WebDriverWait(driver, 10)
-            .until(EC.element_to_be_clickable((By.CLASS_NAME, "next")))
-            .click()
-        )
-
-    df = pd.DataFrame(hotel_list)
-    df.to_csv("hotelReviews.csv", index=0, encoding="utf-8")
+            review_item.update(rate_types)
+            review_list.append(review_item)
+        try:
+            next = (
+                WebDriverWait(driver, 10)
+                .until(
+                    EC.element_to_be_clickable(
+                        (
+                            By.XPATH,
+                            '//div[@class="ui_pagination is-centered"]/a[contains(text(),"Next")]',
+                        )
+                    )
+                )
+                .click()
+            )
+            time.sleep(3)
+        except:
+            df = pd.DataFrame(review_list)
+            df.to_csv("hotelReviews.csv", index=0, encoding="utf-8")
+            break
 
 finally:
     driver.close()
